931d350b43a7ae94dc3556a36914a256b2c18869
NALLA933 <piyush960yogi@gmail.com>
2025-12-30 19:09:03 +0530

    Update utils.py

--- shivu/core/utils.py
+++ shivu/core/utils.py
@@ -1,1 +1,14 @@
-#
+from html import escape
+
+def to_small_caps(text: str) -> str:
+    if not text:
+        return text
+
+    table = str.maketrans(
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+        "ᴀʙᴄᴅᴇғɢʜɪᴊᴋʟᴍɴᴏᴘǫʀsᴛᴜᴠᴡxʏᴢ" * 2
+    )
+    return text.translate(table)
+
+def safe(text):
+    return escape(str(text))
