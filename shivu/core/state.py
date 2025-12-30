481be5e215e4bdd92608215f42708d4606acf272
NALLA933 <piyush960yogi@gmail.com>
2025-12-30 19:09:24 +0530

    Update state.py

--- shivu/core/state.py
+++ shivu/core/state.py
@@ -1,1 +1,14 @@
-#
+import asyncio
+
+locks = {}
+message_counts = {}
+sent_characters = {}
+last_characters = {}
+first_correct_guesses = {}
+spawn_messages = {}
+spawn_message_links = {}
+currently_spawning = {}
+
+MESSAGE_FREQUENCY = 40
+DESPAWN_TIME = 180
+AMV_ALLOWED_GROUP_ID = -1003100468240
